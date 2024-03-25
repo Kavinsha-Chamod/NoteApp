c1016b7ca572002bdd71c3213e595fe3367c550e
Kavinsha Chamod <kavinshachamod2001@gmail.com>
2024-03-25 11:26:31 +0530

    Add CORS configuration for localhost

--- Backend/index.js
+++ Backend/index.js
@@ -7,7 +7,14 @@ const noteRouter = require("./routes/noteRoutes");
 const port = process.env.PORT;
 const app = express();
 
-app.use(cors());
+app.use(cors(
+  {
+    origin: ["http://localhost:3000"],
+    methods: ["GET","POST","PATCH","DELETE"],
+    credentials: true
+  
+  }
+));
 app.use(express.json());
 app.use("/users",userRouter);
 app.use("/notes",noteRouter);
